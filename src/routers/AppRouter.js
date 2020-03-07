1cc313e8eebd0eab503d2aa0d743f70adc474fb6
Vincent Tern <Vincenttern@gmail.com>
2020-03-07 14:28:44 -0800

    update createnote page with styles, header, footer

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,12 +3,12 @@ import { Router, Route, Switch, Link, NavLink } from "react-router-dom";
 import createHistory from "history/createBrowserHistory";
 import Home from "../pages/Home";
 import Contact from "../pages/Contact";
+import CreateNote from "../pages/CreateNote";
 import NotFoundPage from "../pages/NotFoundPage";
 
 // import Navbar from "./components/navbar.component";
 // import ExerciseList from "../components/db/ExerciseList";
 import EditNote from "../components/db/EditNote";
-import CreateNote from "../components/db/CreateNote";
 
 export const history = createHistory();
 
@@ -17,8 +17,8 @@ const AppRouter = () => (
     <Switch>
       <Route path="/" component={Home} exact={true} />
       <Route path="/contact" component={Contact} exact={true} />
-      <Route path="/edit/:id" component={EditNote} exact={true} />
       <Route path="/add-note" component={CreateNote} exact={true} />
+      <Route path="/edit/:id" component={EditNote} exact={true} />
 
       <Route component={NotFoundPage} />
     </Switch>
