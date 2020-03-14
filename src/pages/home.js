8c485a7171eb1c946d3bdd5ee3e04caa7741555a
Vincent Tern <Vincenttern@gmail.com>
2020-03-13 18:42:39 -0700

    test

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../components/Header/Header";
-import HomeTopSection from "../components/Home/HomeTopSection";
 import NotesList from "../components/db/NotesList";
 import Footer from "../components/Footer/Footer";
 
