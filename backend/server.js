1e9f561e3876f413f524c13b511217b71b3c56a7
Vincent Tern <Vincenttern@gmail.com>
2020-03-14 13:56:26 -0700

    test

--- backend/server.js
+++ backend/server.js
@@ -10,22 +10,39 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// cors origin URL - Allow inbound traffic from origin
+corsOptions = {
+  origin: "Your FrontEnd Website URL",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(express.static(path.join(publicPath, "dist")));
 
 const uri = process.env.ATLAS_URI;
-mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-  })
-  .then(res =>
-    console.log("MongoDB database connection established successfully")
-  )
-  .catch(err => console.log(`Error: ${err}`));
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDB is Connected!");
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+};
+// mongoose
+//   .connect(uri, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(res =>
+//     console.log("MongoDB database connection established successfully")
+//   )
+//   .catch(err => console.log(`Error: ${err}`));
 
 app.use("/notes", notesRouter);
 
