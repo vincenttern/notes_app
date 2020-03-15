953f191961f38fa62c26571d7d739d7866ff926f
Vincent Tern <Vincenttern@gmail.com>
2020-03-14 20:06:50 -0700

    test

--- backend/server.js
+++ backend/server.js
@@ -16,18 +16,7 @@ app.use(express.json());
 app.use("/dist", express.static(path.join(publicPath, "dist")));
 
 const uri = process.env.ATLAS_URI;
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(uri, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true
-//     });
-//     console.log("MongoDB is Connected!");
-//   } catch (err) {
-//     console.log(err.message);
-//     process.exit(1);
-//   }
-// };
+
 mongoose
   .connect(uri, {
     useNewUrlParser: true,
